102d3251cee97b5921ff870192675375d7679b72
Online Hacking <45653647+OnlineHacKing@users.noreply.github.com>
2023-02-09 20:21:47 +0530

    Update suman.js

--- suman.js
+++ suman.js
@@ -3,7 +3,7 @@ const fs = require('fs')
 const util = require('util')
 const chalk = require('chalk')
 const { Configuration, OpenAIApi } = require("openai")
-let setting = require('./accesser.json')
+let setting = require('./api.json')
 const BOT_NAME = process.env.BOT_NAME ?? "Harshit ethic";
 
 module.exports = sansekai = async (client, m, chatUpdate, store) => {
